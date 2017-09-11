cfce8984f8f59305371d287422955bab4a606709
zeku87 <nocomment182@gmail.com>
2017-09-12 00:26:42 +0200

    postcss and sass ready

---
+++ gulp/gulpfile.js
@@ -0,0 +1,60 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var watch = require('gulp-watch');
+var inject = require('gulp-inject');
+var autoprefixer = require('autoprefixer');
+var csswring = require('csswring');
+
+gulp.task('html', ['scss'], function(){
+
+    var injectCssFiles = gulp.src('./dist/css/*.css');
+
+    var injectCssOptions = {
+	addRootSlash : false,
+	ignorePath : ['dist'],
+	starttag : '<!-- inject:css -->',
+	endtag : '<!-- endinject -->'
+    };
+    
+    return gulp.src('./src/index.html')
+	.pipe(inject(injectCssFiles, injectCssOptions))
+	.pipe(gulp.dest('./dist'));
+});
+
+gulp.task('scss', function(){
+    var injectScssFiles = gulp.src('./src/scss/modules/*.scss');
+
+    function transformFilePath(filePath){
+	return '@import "' + filePath + '";';
+    }
+
+    var injectScssOptions = {
+	addRootSlash : false,
+	starttag : '// inject:scss',
+	endtag : '// end_inject',
+	transform : transformFilePath
+    };
+
+    var processors = [csswring, autoprefixer];
+    
+    return gulp.src('./src/scss/main.scss')
+	.pipe(inject(injectScssFiles, injectScssOptions))
+	.pipe(sass())
+	.pipe(postcss(processors))
+	.pipe(gulp.dest('./dist/css'));
+    
+});
+
+gulp.task('watch', function(){
+    
+    /*watch('./src/index.html', function(){
+	gulp.start('html');
+    });*/
+
+    watch(['./src/scss/modules/*.scss', './src/scss/main.scss'], function(){
+	gulp.start('html');
+    });
+
+    
+});
